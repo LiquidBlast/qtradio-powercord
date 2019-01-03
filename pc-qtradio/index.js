c02348bfc9821c40ed700178a15699e17532045c
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-03 20:16:07 +0000

    new command

--- pc-qtradio/index.js
+++ pc-qtradio/index.js
@@ -1,18 +1,34 @@
-const Plugin   = require('powercord/Plugin');
-const { get }  = require('powercord/http');
+const Plugin  = require('powercord/Plugin');
+const { get } = require('powercord/http');
 
 module.exports = class qtradio extends Plugin {
-  start () {
+  start() {
     powercord
       .pluginManager
       .get('pc-commands')
       .register(
-        'qttoggle',
+        'toggle',
         'Toggles qtradio.moe playback',
-        'qttoggle',
+        'toggle',
         async () => {
           return await get('http://127.0.0.1:3939/togglePlayback');
         }
-      );
+      )
+
+    powercord
+      .pluginManager
+      .get('pc-commands')
+      .register(
+        'np',
+        'Gives you currently playing qtradio song',
+        'np',
+        async () => {
+          const np = await get('http://127.0.0.1:3939/nowPlaying');
+          return {
+            send: false,
+            result: np.body.artist + ' - ' + np.body.title
+          };
+        }
+      )
   }
 };
