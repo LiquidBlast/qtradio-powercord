3d647a497b4557ffc0e82c7599f46c67fd111883
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-18 20:09:24 +0000

    Support the new unloading thing

--- pc-qtradio/index.js
+++ pc-qtradio/index.js
@@ -2,47 +2,62 @@ const Plugin  = require('powercord/Plugin');
 const { get } = require('powercord/http');
 
 module.exports = class qtradio extends Plugin {
-  start() {
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'toggle',
-        'Toggle qtradio.moe playback',
-        'toggle',
-        async () => {
-          return await get('http://127.0.0.1:3939/togglePlayback');
-        }
-      )
+    start() {
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .register(
+          'toggle',
+          'Toggle qtradio.moe playback',
+          'toggle',
+          async () => {
+            return await get('http://127.0.0.1:3939/togglePlayback');
+          }
+        )
 
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'volume',
-        'Change qtradio.moe volume',
-        'volume <number between 1-100>',
-        async (args) => {
-          return await get(`http://127.0.0.1:3939/changeVolume?input=${args}`);
-        }
-      )
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .register(
+          'volume',
+          'Change qtradio.moe volume',
+          'volume <number between 1-100>',
+          async (args) => {
+            return await get(`http://127.0.0.1:3939/changeVolume?input=${args}`);
+          }
+        )
 
-    powercord
-      .pluginManager
-      .get('pc-commands')
-      .register(
-        'np',
-        'Gives you currently playing qtradio.moe song',
-        'np',
-        async () => {
-          const np                     = await get('https://qtradio.moe/stats');
-          let data                     = np.body.icestats.source[0];
-          if (data === undefined) data = np.body.icestats.source;
-          return {
-            send: false,
-            result: data.artist + ' - ' + data.title
-          };
-        }
-      )
-  }
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .register(
+          'np',
+          'Gives you currently playing qtradio.moe song',
+          'np',
+          async () => {
+            const np = await get('https://qtradio.moe/stats');
+            let data = np.body.icestats.source[0];
+            if (data === undefined) data = np.body.icestats.source;
+            return {
+              send: false,
+              result: data.artist + ' - ' + data.title
+            };
+          }
+        )
+    }
+
+    unload() {
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('toggle');
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('volume');
+      powercord
+        .pluginManager
+        .get('pc-commands')
+        .unregister('np');
+    }
 };
